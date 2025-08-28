0c72714ad374a6ea917d49a68173d0195341534c
Hossam Razaq <razaqhossam.contact@gmail.com>
2025-08-28 16:32:25 +0100

    + add dev prod compose files

--- outline-doc/docker.sh
+++ outline-doc/docker.sh
@@ -2,22 +2,41 @@
 
 set -e
 
+# Load .env variables
+source .env
+
+# Default to 'dev' if OUTLINE_NODE_ENV is unset or 'development'
+case "$OUTLINE_NODE_ENV" in
+  development|"")
+    NODE_ENV_SHORT="dev"
+    ;;
+  production)
+    NODE_ENV_SHORT="prod"
+    ;;
+  *)
+    NODE_ENV_SHORT="$OUTLINE_NODE_ENV"
+    ;;
+esac
+
+COMPOSE_FILE="docker-compose-${NODE_ENV_SHORT}.yml"
+
 case "$1" in
   build)
-    docker compose build
+    docker compose -f $COMPOSE_FILE build
+    docker compose -f $COMPOSE_FILE up -d
     ;;
   up)
-    docker compose up -d
+    docker compose -f $COMPOSE_FILE up -d
     ;;
   down)
-    docker compose down
+    docker compose -f $COMPOSE_FILE down
     ;;
   restart)
-    docker compose down
-    docker compose up -d
+    docker compose -f $COMPOSE_FILE down
+    docker compose -f $COMPOSE_FILE up -d
     ;;
   logs)
-    docker compose logs -f
+    docker compose -f $COMPOSE_FILE logs -f
     ;;
   *)
     echo "Usage: $0 {build|up|down|restart|logs}"
